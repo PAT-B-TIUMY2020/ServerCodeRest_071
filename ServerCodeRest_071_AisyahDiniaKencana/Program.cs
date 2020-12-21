08eef4ebc089c9f6f0a02951cf1cda20ac998f8a
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-12-21 10:24:43 +0700

    Add project files.

---
+++ ServerCodeRest_071_AisyahDiniaKencana/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCodeRest_071_AisyahDiniaKencana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
